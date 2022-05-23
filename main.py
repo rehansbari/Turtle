ce02d7a492fc3bc9042aa7b018b17438af6224ee
Rehan Bari <rbari921@mtroyal.ca>
2022-05-23 13:18:10 -0600

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from turtle import Turtle, Screen
 import random
 
+CIRCLE = 360
+SIDES = 3
+frank = Turtle()
+## This was to make the turtle draw a number shaped from Triangle - Decagon in a repeated sequence
+# def create_shape(num_sides):
+#     global SIDES
+#     R = random.random()
+#     B = random.random()
+#     G = random.random()
+#     frank.color(R, G, B)
+#     for _ in range(num_sides):
+#         frank.right(CIRCLE/SIDES)
+#         frank.forward(100)
+#     SIDES += 1
+#
+# for _ in range(9):
+#     create_shape(SIDES)
+
+#Using the Turtle methods to make the turtle generate a random walk on screen
 timmy = Turtle()
 timmy.shape("turtle")
 
@@ -31,5 +50,6 @@ while i < 100:
     else:
         random_walk2()
     i += 1
+
 screen = Screen()
 screen.exitonclick()
